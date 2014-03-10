bc6c9a2ead60794713e8a4d33cfaa9a54fbdaa9a
MSeal <mseal007@gmail.com>
2014-03-10 08:42:30 -0700

    Added allset to init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,4 @@
-import glob
-import os
-# Hack to get setup tools to correctly include all python files
-__all__ = [module.split(os.path.sep)[-1].split('.')[0] 
-           for module in glob.glob(os.path.dirname(__file__)+'/*.py') if '__' not in module]
-__all__.extend([module.split(os.path.sep)[-2] 
-                for module in glob.glob(os.path.dirname(__file__)+'/*/__init__.py')])
-del os
-del glob
+import allset
+allset.set_all_submodules(globals())
+allset.bind_all_submodules(globals())
+del allset
